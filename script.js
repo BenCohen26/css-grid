7dea58e05aab8bd5df8935a5f5dba79315b4a653
Ben Cohen <ben.cohenvi@gmail.com>
2019-03-20 11:59:34 +0200

    Random Changes

---
+++ script.js
@@ -0,0 +1,9 @@
+function Add(a, b) {
+    return a + b;
+}
+
+function Subreact(c, d) {
+    let subtractionValue = c - d;
+    console.log(`The result of the subtraction opertation on both of the numbers gotten are ${subtractionValue}`);
+    return subtractionValue;
+}
